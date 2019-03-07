8552ebface5c667772f253601e45fb4818b0afd1
0dsabella <42749703+0dsabella@users.noreply.github.com>
2019-03-07 13:58:26 -0600

    Add files via upload

---
+++ toggleFunctions.js
@@ -0,0 +1,43 @@
+function green25(){
+    const doc = document;
+    let divs = doc.querySelectorAll("div");
+    
+    for(let k=0; k<divs.length; k++){
+        divs[k].classList.toggle('green25')
+    }
+
+}
+
+
+function green50(){
+
+
+}
+
+
+function green75(){
+
+
+}
+
+
+function green100(){
+
+
+}
+
+
+function smallToggle(){
+
+
+}
+
+
+function mediumToggle(){
+
+}
+
+
+function largeToggle(){
+
+}
